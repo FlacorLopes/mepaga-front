02531735160f64fc5f69a5926cb35ced2dd3a5e0
Flávio Lopes <flavioloppes@outlook.com>
2022-03-15 00:45:24 -0300

    (feat) add metadata to news and help

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -17,12 +17,43 @@
   </div>
 </template>
 <script lang="ts">
+import { useMeta } from 'quasar';
 import { defineComponent } from 'vue';
+
+const metaData = {
+  title: 'Ajuda',
+  titleTemplate: (title: string) =>
+    `${title} - MePaga | O lugar certo para organizar suas faturas`,
+
+  // meta tags
+  meta: {
+    description: {
+      name: 'description',
+      content: 'Importe a fatura do seu cartão em PDF e organize por compra.',
+    },
+    keywords: {
+      name: 'keywords',
+      content: 'fatura,cartão,pdf,importar fatura,dividir compras,compras,',
+    },
+    equiv: {
+      'http-equiv': 'Content-Type',
+      content: 'text/html; charset=UTF-8',
+    },
+    // note: for Open Graph type metadata you will need to use SSR, to ensure page is rendered by the server
+    ogTitle: {
+      property: 'og:title',
+      // optional; similar to titleTemplate, but allows templating with other meta properties
+      template(ogTitle: string) {
+        return `${ogTitle} - MePaga | O lugar certo para organizar suas faturas`;
+      },
+    },
+  },
+};
 
 export default defineComponent({
   name: 'Help',
   setup() {
-    //
+    useMeta(metaData);
   },
 });
 </script>
